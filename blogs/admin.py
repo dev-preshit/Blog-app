30cd10464862c5c58b14e3c2456a10a0af90ba31
dev-preshit <preshit.kamble17705@gmail.com>
2026-05-27 00:04:40 +0530

    Slags and Admin table

--- blogs/admin.py
+++ blogs/admin.py
@@ -3,5 +3,12 @@ from .models import Category, Blog
 
 # Register your models here.
 
+class BlogAdmin(admin.ModelAdmin):
+
+    prepopulated_fields = {'slug' : ('title',)}
+    list_display = ('title', 'category', 'author', 'status', 'is_featured')
+    search_fields = ('id', 'title', 'category__category_name', 'status', 'is_featured' )
+    list_editable = ('status','is_featured')
+
 admin.site.register(Category)
-admin.site.register(Blog)
+admin.site.register(Blog,BlogAdmin)
